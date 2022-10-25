b240034d719ac4d781793a1b90d6d97a24162363
perintyler <perintyler@gmail.com>
2022-10-25 15:18:08 -0400

    function for getting unique grouptext identifier using a group text display name

--- grouptext.py
+++ grouptext.py
@@ -9,6 +9,8 @@ databse for a specific group text.
 import os
 import pwd
 
+import pandas as pd
+
 def get_path_to_imessage_database() -> str:
   """
   For each OSX user, all imessages are stored in a local SQLite database stored at: "~/Library/Messages/chat.db".
@@ -19,3 +21,20 @@ def get_path_to_imessage_database() -> str:
   osx_user_account = pwd.getpwuid(osx_user_id)
   osx_username = osx_user_account.pw_name
   return os.path.join('/Users', osx_username, 'Library', 'Messages', 'chat.db')
+
+def get_chat_identifier(group_name: str, connection: sqlite3.Connection) -> str:
+  """
+  Every group text has a unique identifier, since multiple group texts can have the same
+  display name. It will look something like this: 'chat131457465125669997'
+
+  - group_name: the display name shown at the top when a group text is selected
+  - connection: an open connection to the imessage SQLite database
+  """
+  chat_query = pd.read_sql_query(f"SELECT chat_identifier FROM chat WHERE display_name = '{group_name}'", connection)
+
+  if len(chat_query) == 0:
+    raise ValueError(f'No group texts exists for group name {group_name}')
+  if len(chat_query) > 1:
+    raise ValueError(f'There is more than one group for group name {group_name}')
+
+  return chat_query['chat_identifier'][0]
