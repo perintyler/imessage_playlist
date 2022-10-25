cdf31645c0aa94b2444f18298b97b6e38467b919
perintyler <perintyler@gmail.com>
2022-10-25 15:22:35 -0400

    test for getting a unique ID for a group chat

--- test.py
+++ test.py
@@ -2,3 +2,18 @@
   imessage_playlist.tests
   ~~~~~~~~~~~~~~~~~~~~~~~
 """
+
+from .grouptext import (
+  get_path_to_imessage_database,
+  get_chat_identifier,
+)
+
+import sqlite3
+
+NAME_OF_TEST_IMESSAGE_GROUPCHAT = 'test group chat'
+
+def test_get_chat_identifier():
+  connection = sqlite3.connect(get_path_to_imessage_database())
+  assert get_chat_identifier(NAME_OF_TEST_IMESSAGE_GROUPCHAT, connection) \
+     == 'chat131457465125669997'
+
