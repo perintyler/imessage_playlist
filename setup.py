abd524db5d9a920d9972352129627ed68eb9f64e
perintyler <perintyler@gmail.com>
2022-10-25 17:48:03 -0400

    tell `setup.py` that source files are in the repo's root directory

--- setup.py
+++ setup.py
@@ -5,3 +5,12 @@
 This file contains setup instructions for the `imessage_playlist` package
 for easy installation.
 """
+
+from setuptools import setup
+
+PACKAGE_NAME = 'imessage_playlist'
+
+setup(
+  name=PACKAGE_NAME,
+  package_dir={PACKAGE_NAME: '.'}, # source files are in the repos's root directory
+)
