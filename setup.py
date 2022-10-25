ab18fd2170e2bd61ec4de8b4942420e32b2b9533
perintyler <perintyler@gmail.com>
2022-10-25 17:57:02 -0400

    install dependencies in `setup.py` by reading `requirements.txt`

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ from setuptools import setup
 
 PACKAGE_NAME = 'imessage_playlist'
 
+
+with open('./requirements.txt') as f:
+  requirements = f.read().splitlines()
+
 setup(
   name=PACKAGE_NAME,
   package_dir={PACKAGE_NAME: '.'}, # source files are in the repos's root directory
+  install_requires=requirements # requirements outlined in `requirements.txt`
 )
