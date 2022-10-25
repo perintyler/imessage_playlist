81494fb79ed8f441251d74cf972478abaeaedd23
perintyler <perintyler@gmail.com>
2022-10-25 15:16:56 -0400

    create grouptext module

---
+++ grouptext.py
@@ -0,0 +1,7 @@
+""""
+  imessage_playlist.grouptext
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module is responsible for querying imessages from the local SQLite 
+databse for a specific group text.
+"""
