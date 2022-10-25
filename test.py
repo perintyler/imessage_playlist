06fb3bc72161c0caad63f73c053ba2830a8b1cba
perintyler <perintyler@gmail.com>
2022-10-25 15:23:47 -0400

    test for getting path to local imessage database file

--- test.py
+++ test.py
@@ -12,6 +12,10 @@ import sqlite3
 
 NAME_OF_TEST_IMESSAGE_GROUPCHAT = 'test group chat'
 
+def test_get_path_to_imessage_database():
+  assert get_path_to_imessage_database() \
+      == '/Users/tylerperin/Library/Messages/chat.db'
+
 def test_get_chat_identifier():
   connection = sqlite3.connect(get_path_to_imessage_database())
   assert get_chat_identifier(NAME_OF_TEST_IMESSAGE_GROUPCHAT, connection) \
