399c5a966438a75fe5bd51de4b82c751a7c4341c
perintyler <perintyler@gmail.com>
2022-10-31 17:19:38 -0400

    create a function for creating a playlist from a group chat display name

--- curate.py
+++ curate.py
@@ -6,6 +6,16 @@ This module is responsible for creating a spotify playlist with a collection
 of track links.
 """
 
+import os
+
+from .grouptext import get_texts_for_group
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def get_track_links_from_grouptext(group_name):
   """
   """
@@ -17,3 +27,22 @@ def get_track_links_from_grouptext(group_name):
         spotify_links.append(word)
 
   return spotify_links
+
+def create_playlist_from_grouptext(playlist_name, group_name):
+  """
+  """
+  track_links = get_track_links_from_grouptext(group_name)
+
+  spotify_client = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    scope='playlist-modify-private', 
+    client_id=os.getenv('SPOTIFY_CLIENT_ID'),
+    client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'),
+    redirect_uri=os.getenv('SPOTIFY_REDIRECT_URI')
+  ))
+
+  user_id = spotify_client.current_user()['id']
+
+  playlist = spotify_client.user_playlist_create(user_id, playlist_name, public=False, collaborative=False)
+
+  spotify_client.playlist_add_items(playlist['id'], items=track_links)
+
