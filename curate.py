4e5ae1cf5f6cd087c46cf218fe185555e740421d
perintyler <perintyler@gmail.com>
2022-10-31 17:18:58 -0400

    create function for getting track links from raw text messages

--- curate.py
+++ curate.py
@@ -5,3 +5,15 @@
 This module is responsible for creating a spotify playlist with a collection
 of track links.
 """
+
+def get_track_links_from_grouptext(group_name):
+  """
+  """
+  spotify_links = []
+
+  for text_message in get_texts_for_group(group_name):
+    for word in text_message.split(' '):
+      if word.startswith('https://open.spotify.com/track'):
+        spotify_links.append(word)
+
+  return spotify_links
