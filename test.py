b3ec05997d364595da6edbd69d8bab0b9a95f9e9
perintyler <perintyler@gmail.com>
2022-11-03 18:01:56 -0400

    fix `test_get_track_links_from_grouptext`
    the test should expect a set, not a list

--- test.py
+++ test.py
@@ -47,9 +47,9 @@ def test_get_texts_for_group():
   ]
 
 def test_get_track_links_from_grouptext():
-  assert get_track_links_from_grouptext(NAME_OF_TEST_IMESSAGE_GROUPCHAT) == [
+  assert get_track_links_from_grouptext(NAME_OF_TEST_IMESSAGE_GROUPCHAT) == {
     'https://open.spotify.com/track/3DbBgbnDUOml329rAp2Tbr?si=de627e1538c242e7', 
     'https://open.spotify.com/track/1x0WbCpdSPdwVoTCdJ4pPZ?si=7721bb5c607f4d7e', 
     'https://open.spotify.com/track/5lw8Mgb4LyhriPIC86gV6e?si=cb9c17509a5e457a', 
     'https://open.spotify.com/track/5xTtaWoae3wi06K5WfVUUH?si=gxXVvjBITqSvFIyu4mfR7A'
-  ]
+  }
